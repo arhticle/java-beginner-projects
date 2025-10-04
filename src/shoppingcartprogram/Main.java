610466c6cd787aba7488fe6ba3c406148a930f28
Gustas Jarusevicius <arhticle@gmail.com>
2025-10-04 21:19:40 +0100

    First version of simple shopping cart program

--- src/shoppingcartprogram/Main.java
+++ src/shoppingcartprogram/Main.java
@@ -7,6 +7,43 @@ public class Main {
     public static void main(String[] args) {
 
         // shopping cart program beginner project.
+        Scanner scanner = new Scanner(System.in);
+
+
+        // declaring variables for the program.
+        String item;
+        double price;
+        int quantity;
+        char currency = '£';
+        double total;
+
+
+        // asking user for inputs for their shopping cart.
+        System.out.print("Please type the name of the item you would like to purchase: ");
+        item = scanner.nextLine();
+        System.out.println("What is the price of this item?: ");
+        price = scanner.nextDouble();
+        System.out.print("How much of this item would you like to buy?: ");
+        quantity = scanner.nextInt();
+
+
+        // thank you message
+        System.out.println("Thank you.");
+
+
+        // calculation on how to work out the total.
+        total = price * quantity;
+
+
+        // output based on what the user gave + logic code.
+        System.out.println("\nYou have chosen " + quantity + item + "/s.");
+        System.out.println("Your total is " + currency + total + "!");
+        System.out.println("\nThank you for using our program.");
+
+
+        // end of program code.
+        scanner.close();
+
 
     }
 }

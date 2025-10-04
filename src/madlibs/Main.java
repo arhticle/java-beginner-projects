3dd84a34430115883d7f46e754024dc83a15f083
Gustas Jarusevicius <arhticle@gmail.com>
2025-10-04 18:32:48 +0100

    Added madlibs project + beginner starting code

---
+++ src/madlibs/Main.java
@@ -0,0 +1,17 @@
+package madlibs;
+
+import java.util.Scanner;
+
+public class Main {
+    static void main(String[] args) {
+
+        // madlibs beginner project game.
+
+        Scanner scanner = new Scanner(System.in);
+
+
+
+        scanner.close();
+
+    }
+}

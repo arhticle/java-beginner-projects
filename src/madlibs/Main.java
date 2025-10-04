7ed82a89124bbd1b52e64bdcf0bbc8284a5980ca
Gustas Jarusevicius <arhticle@gmail.com>
2025-10-04 19:19:06 +0100

    MadLibs game logic finished; fixed errors and everything else

--- src/madlibs/Main.java
+++ src/madlibs/Main.java
@@ -25,30 +25,44 @@ public class Main {
 
         // asking user to input their answer for the specific section of the game.
 
-        System.out.println("Please input your first noun (person/place): ");
-        System.out.println("Please input your first adjective (describing something): ");
-        System.out.println("Please input your first adjective (describing something): ");
-        System.out.println("Please input your first adjective (describing something): ");
-        System.out.println("Please input your first adjective (describing something): ");
-        System.out.println("Please input your first adjective (describing something): ");
-        System.out.println("Please input your first adjective (describing something): ");
-        System.out.println("Please input your first adjective (describing something): ");
+        System.out.println("Hello! Welcome to the MadLibs game!\n");
 
+        System.out.print("Please input your first noun (person/place): ");
+        noun1 = scanner.nextLine();
+        System.out.print("Please input your first adjective (describing something): ");
+        adjective1 = scanner.nextLine();
+        System.out.print("Please input your age: ");
+        age1 = scanner.nextInt();
+        scanner.nextLine(); // added this to avoid common issue with overlapping i/o.
+        System.out.print("Please input your second adjective (describing something): ");
+        adjective2 = scanner.nextLine();
+        System.out.print("Please input your GPA (grade): ");
+        GPA = scanner.nextDouble();
+        scanner.nextLine(); // added again to avoid issue with overlapping i/o.
+        System.out.print("Please input your car choice: ");
+        car1 = scanner.nextLine();
+        System.out.print("Please input your price choice (£): ");
+        price1 = scanner.nextDouble();
+        scanner.nextLine(); // added again to avoid issue with overlapping i/o.
+        System.out.print("Please input your first verb (action): ");
+        verb1 = scanner.nextLine();
+        System.out.print("Please input your third adjective (describing something): ");
+        adjective3 = scanner.nextLine();
+        System.out.print("Please input your fourth adjective (describing something): ");
+        adjective4 = scanner.nextLine();
 
 
         // the main output of the game after user input has been received.
 
-        System.out.println("Today I went to a " + noun1 + "!");
+        System.out.println("\nToday I went to a " + noun1 + "!");
         System.out.println("Inside this " + noun1 + " I " + adjective1 + " found something interesting.");
         System.out.println("There, I was asked to input my age which was " + age1 + ".");
-        System.out.println("The place was acting very" + adjective2 + "...");
-        System.out.println("After several seconds I got asked for my... GPA?! So I input " + GPA + "!");
-        System.out.println("After several seconds, I got asked for my favourite car choice, which was " + car1 + ".");
-        System.out.println("Then asked for my desired price, which I said ") + price1 + "!";
+        System.out.println("The place was acting very " + adjective2 + "...");
+        System.out.println("After several seconds I got asked for my... GPA?! So I said " + GPA + "!");
+        System.out.println("Then, I got asked for my favourite car choice, which was " + car1 + ".");
+        System.out.println("After I got asked for my desired price, which I said " + price1 + "!");
         System.out.println("At the end, it calculated my results, I " + verb1 + "very " + adjective3 + ".");
-        System.out.println("Finally, their result told me I was " + adjective 4 + "!" + " Can you believe it?");
-
-
+        System.out.println("Finally, their result told me I was " + adjective4 + "!" + " Can you believe it?");
 
 
         scanner.close();
